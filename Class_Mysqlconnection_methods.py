6614c805d452f366e5f7e44fe0a88226b703dd8e
Alireza Shamsi <almator5@gmail.com>
2022-05-21 01:04:09 +0430

    Add some methods

--- Class_Mysqlconnection_methods.py
+++ Class_Mysqlconnection_methods.py
@@ -1,3 +1,4 @@
+from os import kill
 import mysql.connector
 #Connect as alireza
 con = mysql.connector.MySQLConnection(user= 'alireza', database= 'test')
@@ -10,4 +11,26 @@ con.reconnect()
 #NOTE: commit must be enter every time you change a query from databse using poython this is not automaticaly
 mysql.connector.MySQLConnection.cursor.execute("INSERT INTO emoployees (first name) VALUES (%s)", ('alireza'))
 con.commit()
+
+#Cursor method
+mysql.connector.MySQLConnection.cursor(buffered=None, raw=None, cursor_class=None)
+
+#cmd methods:
+#1.Write debugging in log(must be SUPER user)
+mysql.connector.MySQLConnection.cmd_debug()
+
+#2.use defined database in there "AlirezaBase" initial database
+mysql.connector.MySQLConnection.cmd_init_db(AlirezaBase)
+
+#Checks connection to dbmsServer
+#NOTE: DONT USE IT DIRECTLY
+mysql.connector.MySQLConnection.cmd_ping()
+
+#Show informatin in INFORMATION_SCHEMA in database
+mysql.connector.MySQLConnection.cmd_process_info()
+
+
+#first command kill 123 thread
+#second command delete thread 123
+con.cmd_process_kill(123)
 
