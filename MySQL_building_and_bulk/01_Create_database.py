1df0c174ac593ec3683e0a536dc025c7c27d7005
Alireza Shamsi <almator5@gmail.com>
2022-05-25 19:08:42 +0430

    Connect python to MySQL

---
+++ MySQL_building_and_bulk/01_Create_database.py
@@ -0,0 +1,14 @@
+import mysql.connector
+conn = mysql.connector.connect(
+    host= 'localhost',
+    user = 'ashix',
+    passwd = 'P@ssw0rd'
+)
+
+myCursor = conn.cursor()
+if myCursor:
+    myCursor.execute("CREATE DATABASE myDatabase")
+    print("myDatabase is create")
+    
+else:
+    print('Error!')
