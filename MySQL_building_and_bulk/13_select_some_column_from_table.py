0e45e26d5a97744b3ca02dcc60abc591ea173339
Alireza Shamsi <almator5@gmail.com>
2022-05-30 06:34:06 +0430

    Select some column from table

---
+++ MySQL_building_and_bulk/13_select_some_column_from_table.py
@@ -0,0 +1,20 @@
+from operator import imod
+
+
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host = 'localhost',
+    user = 'ashix',
+    passwd = 'P@ssw0rd',
+    database = 'publisher',
+)
+
+myCursor = conn.cursor()
+sql = 'select pName, Tel from publishers'
+myCursor.execute(sql)
+
+myResult = myCursor.fetchall()
+
+for x in myResult:
+    print(x)
