8935f522d6472441a328c2e3d7a81641237614c3
Alireza Shamsi <almator5@gmail.com>
2022-06-01 06:41:28 +0430

    fetch limite rows from table

---
+++ MySQL_building_and_bulk/19_use_limit.py
@@ -0,0 +1,18 @@
+from ast import Num
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host = 'localhost',
+    user = 'ashix',
+    passwd = 'P@ssw0rd',
+    database = 'publisher',
+)
+
+myCursor = conn.cursor()
+
+num = input("Enter number of rows that you want : ")
+sql = 'select title, price from books limit ' + num
+myCursor.execute(sql)
+myResult =  myCursor.fetchall()
+for x in myResult:
+    print(x)
